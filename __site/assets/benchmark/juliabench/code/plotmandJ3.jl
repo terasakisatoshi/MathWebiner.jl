642508b2d352eb1a26a0ed2671c67a5d8d33de71
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-04-07 01:32:08 +0900

    franklin-update

--- __site/assets/benchmark/juliabench/code/plotmandJ3.jl
+++ __site/assets/benchmark/juliabench/code/plotmandJ3.jl
@@ -4,8 +4,9 @@ Tarpath="/tmp/resultJ3.txt"
 run(`bash -c "cmp --silent $Refpath $Tarpath || echo \"files are different\""`)
 
 using Plots
+
 using DelimitedFiles
 img = readdlm(Tarpath, ',', Int)
 M,N = img |> size
 p = heatmap(1:N, 1:M, img,aspect_ratio=1, size=(400, 400))
-savefig(p, joinpath(@OUTPUT, "mandJ3.png")) #hide
+savefig(p, joinpath(@OUTPUT, "mandJ3.png"))
